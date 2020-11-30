977d7fc546fdd6a6bc96fe5797ab550aaed8628d
Promise Harry <chinecherempharry@gmail.com>
2020-11-30 12:02:41 +0100

    added to the footer and re-deployment

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy;</Col>
+          <Col className='text-center py-3'>Copyright &copy; Harry 2020</Col>
         </Row>
       </Container>
     </footer>
